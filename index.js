d9c8aa1408bcdaa1a6c7766983d18886e3a6793a
moose-hub <infamollagames@gmail.com>
2024-02-19 11:38:00 +0000

    selected elements with class content

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("linked");
 const t1Title = document.getElementById("title");
 console.log(t1Title);
+
+const contentElements = document.getElementsByClassName("content");
+console.log(contentElements[0]);
+console.log(contentElements[1]);
