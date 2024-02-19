7bbb07315fd09f04aee4be1c2658b1c8427903d2
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-19 12:28:44 +0000

    2e

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-console.log("linked");
-const t1Title = document.getElementById("title");
-console.log(t1Title);
+// console.log("linked");
+// const t1Title = document.getElementById("title");
+// console.log(t1Title);
 
-const contentElements = document.getElementsByClassName("content");
-console.log(contentElements[0]);
-console.log(contentElements[1]);
+// const contentElements = document.getElementsByClassName("content");
+// console.log(contentElements[0]);
+// console.log(contentElements[1]);
 
-const allLists = document.getElementsByTagName("li");
+// const allLists = document.getElementsByTagName("li");
 
-console.log(allLists);
+// console.log(allLists);
 
-//  Ticket 1 D ****************
+// //  Ticket 1 D ****************
 
-const firstHighlight = document.querySelector(".highlight");
-console.log(firstHighlight);
+// const firstHighlight = document.querySelector(".highlight");
+// console.log(firstHighlight);
 
-const highlightList = document.querySelectorAll(".highlight");
-console.log(highlightList);
+// const highlightList = document.querySelectorAll(".highlight");
+// console.log(highlightList);
 
-// Ticket 2 ****************
+// // Ticket 2 ****************
 
-const firstListItem = document.querySelector("#itemList li");
+// const firstListItem = document.querySelector("#itemList li");
 
-const parentList = firstListItem.parentNode;
+// const parentList = firstListItem.parentNode;
 
-console.log(parentList);
+// console.log(parentList);
 
-// ticket 2b ****************
+// // ticket 2b ****************
 
-const itemList = document.querySelector("#itemList");
+// const itemList = document.querySelector("#itemList");
 
-console.log(itemList.childNodes);
+// console.log(itemList.childNodes);
 
-// ticket 2c ****************
+// // ticket 2c ****************
 
-const ulItemList = document.getElementById("itemList");
-console.log("ticke2c");
-console.log(ulItemList.children);
+// const ulItemList = document.getElementById("itemList");
+// console.log("ticke2c");
+// console.log(ulItemList.children);
 
-// ticke 2d ***********
+// // ticke 2d ***********
 
-console.log(ulItemList.firstChild);
-console.log(ulItemList.lastElementChild);
+// console.log(ulItemList.firstChild);
+// console.log(ulItemList.lastElementChild);
+
+// // ticket 2e **********
+
+const cherryLi = document.querySelector("li.highlight");
+
+console.log("newline");
+console.log(cherryLi);
+console.log(cherryLi.nextSibling);
+console.log(cherryLi.previousSibling);
+console.log(cherryLi.nextElementSibling);
