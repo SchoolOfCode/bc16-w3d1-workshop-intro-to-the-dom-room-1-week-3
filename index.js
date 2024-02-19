3e2acc01bd27bcf790274ed734c6cc9c61e902d2
moose-hub <infamollagames@gmail.com>
2024-02-19 12:42:49 +0000

    finished all tickets

--- index.js
+++ index.js
@@ -45,10 +45,46 @@
 
 // // ticket 2e **********
 
-const cherryLi = document.querySelector("li.highlight");
+// const cherryLi = document.querySelector("li.highlight");
 
-console.log("newline");
-console.log(cherryLi);
-console.log(cherryLi.nextSibling);
-console.log(cherryLi.previousSibling);
-console.log(cherryLi.nextElementSibling);
+// console.log("newline");
+// console.log(cherryLi);
+// console.log(cherryLi.nextSibling);
+// console.log(cherryLi.previousSibling);
+// console.log(cherryLi.nextElementSibling);
+
+//  ticket 3a ...............
+const pageTitle = document.getElementById("title");
+
+pageTitle.style.color = "red";
+pageTitle.style.fontSize = "30px";
+
+
+const newParagraph = document.createElement("p");
+
+newParagraph.textContent = "This is a dynamically added paragraph";
+
+document.body.appendChild(newParagraph);
+
+
+// ticket 3b .........
+
+const secondPContent = document.querySelector("p.content").nextElementSibling;
+
+secondPContent.setAttribute("title", "Hover over me!");
+
+// ticket 3e *********************
+
+const ulItemListID = document.getElementById("itemList");
+
+ulItemListID.classList.add("styled-list");
+
+const liContentBanana = document.querySelector("li.highlight").previousElementSibling;
+
+liContentBanana.classList.remove("list-item");
+
+
+// ticket 3f **********************
+const liContentDate = document.querySelector("li.highlight").nextElementSibling;
+
+liContentDate.parentNode.removeChild(liContentDate);
